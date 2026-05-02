96cd00b005d5f953097d18f91b486906dbdcba4a
DERICK798 <neshderrick798@gmail.com>
2026-05-02 17:36:19 +0300

    middle ware

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Authorization header missing' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.error('JWT VERIFY ERROR:', err.message);
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    req.user = decoded; // { id, role }
+    next();
+  });
+};
