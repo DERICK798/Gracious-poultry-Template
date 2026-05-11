4659fa7e4deb7f01f7cabb2a63bbb45478258e20
DERICK798 <neshderrick798@gmail.com>
2026-05-11 17:27:36 +0300

    uploads errors

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use(session({
 // frontend files
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'public')));
 // Serving uploads from the root project directory to match Multer config
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, '../uploads')));
 
 // APIs
 app.use('/api/auth', authRoutes);
